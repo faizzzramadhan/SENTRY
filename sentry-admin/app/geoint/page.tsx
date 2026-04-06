439758557cf90595793f611031aecb21e2cf2b65
Ikbarwisuda <wisuda871@gmail.com>
2026-04-06 19:05:48 +0700

    update geoint 1

---
+++ sentry-admin/app/geoint/page.tsx
@@ -0,0 +1,10 @@
+import styles from './geoint.module.css';
+
+export default function GeoIntPage() {
+    return (
+        <div className={styles.container}>
+            <h1>GeoInt</h1>
+            <p>Welcome to GeoInt</p>
+        </div>
+    );
+}
