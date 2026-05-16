fc6db87705527987c80bc31e3961db7e4eb3fbf3
rifcha syabani fatullah <rifchasyabani30@gmail.com>
2026-05-16 18:54:53 +0700

    updte index.js RFC

--- sentry-backend/src/routes/index.js
+++ sentry-backend/src/routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const userRoutes = require("./user");
 const osintRoutes = require("./osint");
+const jenisBencanaRoutes = require("./jenis_bencana");
+const namaBencanaRoutes = require("./nama_bencana");
 
 const humintRoutes = require("./humint/laporan");
 const adminHumintRoutes = require("./humint/admin_laporan");
@@ -16,6 +18,11 @@ const logAktivitasRoutes = require("./logAktivitasRoutes");
 router.use("/user", userRoutes);
 router.use("/osint", osintRoutes);
 router.use("/log-aktivitas", logAktivitasRoutes);
+
+router.use("/jenis-bencana", jenisBencanaRoutes);
+router.use("/nama-bencana", namaBencanaRoutes);
+router.use("/humint/jenis-bencana", jenisBencanaRoutes);
+router.use("/humint/nama-bencana", namaBencanaRoutes);
 
 router.use("/humint", rekapPdfRoutes);
 router.use("/humint", downloadPdfRoutes);
